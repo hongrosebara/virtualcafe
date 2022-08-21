ae94fc6ca17193a3200708838a543ddc3cfa3551
Hong Le <hongrosebara@gmail.com>
2022-08-21 13:58:40 +0100

    first commit

---
+++ components/layout/Footer.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function footer({}) {
+  return (
+    <footer className='bg-fuchsia-100 mt-8 py-4'>
+      <div className='container mx-auto flex justify-center'>
+        &copy; 2022 DailyDevTips
+      </div>
+    </footer>
+  );
+}
