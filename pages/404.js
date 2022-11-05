f251ffdbc77d68923a47e2bec1d5af99fa3509d4
Hong Le <hongrosebara@gmail.com>
2022-11-05 15:17:35 +0000

    404 redirects added vercel.json

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
+import Image from 'next/image'
+
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div className="section-main m-6">
+      <Image
+        src="/404.svg"
+        layout='fill'
+        sizes="(max-width: 768px) 100vw,
+                (max-width: 1200px) 50vw,
+                33vw"
+      />
+    </div>
   )
 }
