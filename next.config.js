cb4b5927d0e7635fcb939c87c7e9cb40dbee38e6
Hong Le <hongrosebara@gmail.com>
2022-11-05 18:47:37 +0000

    redirects

--- next.config.js
+++ next.config.js
@@ -5,66 +5,19 @@ const withPWA = require('next-pwa')({
   // scope: '/app',
   // sw: 'service-worker.js',
   //...
+})
+
+const redirects = {
   async redirects() {
     return [
       {
-        source: '/how-long-do-coffee-beans-last/',
+        source: '/about',
         destination: '/',
         permanent: true,
       },
-      {
-        source: '/aeropress-vs-french-press-difference/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/difference-between-french-and-italian-roast/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/best-espresso-beans-reviews/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/how-to-make-a-spanish-latte/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/how-to-make-vietnamese-coffee/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/espresso-beans-vs-coffee-beans/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/what-everybody-ought-to-know-about-coffees-effect-on-the-body/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/7-wonders-of-coffee/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/best-espresso-machine-under-1000-reviews/',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/how-to-make-americano-coffee/',
-        destination: '/',
-        permanent: true,
-      },
-    ]
+    ];
   },
-})
+};
 
 module.exports = withPWA({
   // next.js config
@@ -73,5 +26,14 @@ module.exports = withPWA({
     config.resolve.fallback = { fs: false };
 
     return config;
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true
+      }
+    ]
   }
 })
