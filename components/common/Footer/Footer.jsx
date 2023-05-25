988a5f194aff754515d27fbc613341d1cc3f173d
Hong Le <hongrosebara@gmail.com>
2023-05-25 22:36:59 +0700

    changed color-on light dark theme

--- components/common/Footer/Footer.jsx
+++ components/common/Footer/Footer.jsx
@@ -6,19 +6,20 @@ import { navItemsList } from "@/components/utils/getNavItems";
 export default function Footer() {
 
   return (
-    <footer className="footer-wrapper bg-primary-dk">
+    <footer className="footer-wrapper bg-surface">
       <div className="flex flex-col items-center justify-between lg:flex-row">
-        <Logo fillColor="var(--color-on-lt)" classNames="text-on-lt" />
+        <Logo fillColor="var(--color-on-dk)" classNames="text-on-dk" />
         <nav className={s["footer-nav"]}>
-          <ul className="py-16 flex flex-col lg:flex-row justify-center items-center text-center space-x-6 space-y-4 lg:space-y-0 text-on-lt">
+          <ul className="py-16 flex flex-col lg:flex-row justify-center items-center text-center space-y-4 lg:space-x-6 lg:space-y-0">
             {navItemsList.map((item) => (
-              <li key={item.id}>
+              <li key={item.id} className="text-on-dk hover:text-gray">
                 <Link href={`${item.slug}`}>{item.name}</Link>
               </li>
             ))}
           </ul>
         </nav>
-        <SocialNav fillColor="var(--color-on-lt)" />
+        <SocialNav 
+          fillColor="var(--color-on-dk)" />
       </div>
     </footer>
   );
--- components/common/SocialNav/SocialNav.jsx
+++ components/common/SocialNav/SocialNav.jsx
@@ -1,11 +1,14 @@
 import { Facebook, Instagram, Twitter } from "@/components/icons";
+import s from "./SocialNav.module.scss";
 
 const SocialNav = ({ fillColor }) => {
   return (
-    <div className="flex items-center justify-end space-x-4 text-accent-lt">
-      <Facebook fillColor={`${fillColor}`} />
-      <Instagram fillColor={`${fillColor}`} />
-      <Twitter fillColor={`${fillColor}`} />
+    <div className={s["social-nav"]}>
+      <div className="flex items-center justify-end space-x-4 cursor-pointer">
+        <Facebook fillColor={`${fillColor}`} />
+        <Instagram fillColor={`${fillColor}`} />
+        <Twitter fillColor={`${fillColor}`} />
+      </div>
     </div>
   );
 };
